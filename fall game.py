dbe6e1da2983a5fab7e54143396b407a69177b7d
Henrich <67014285+henrich-print@users.noreply.github.com>
2020-11-25 14:47:36 +0100

    Update fall game.py

--- fall game.py
+++ fall game.py
@@ -1,42 +1,77 @@
 from microbit import *
 from time import sleep
+import random
 
 class Player:
 	def __init__(self, app_self):
 		self.app_self = app_self
-		self.pos = {"x":4, "y": 3, "width": 1, "height": 1}
+		self.pos = {"x":4, "y": 3}
 		self.skin = 9
+		self.score = 0
 
 	def loop(self):
 		if button_a.is_pressed():
 			self.pos["x"] -= 1
-			sleep(.5)
+			sleep(.25)
 
 		elif button_b.is_pressed():
 			self.pos["x"] += 1
-			sleep(.5)
+			sleep(.25)
+
+		if self.pos["x"]-2*self.pos["y"] == self.app_self.coin.pos["x"]-2*self.app_self.coin.pos["y"]:
+			self.score += 1
+			self.app_self.coin.get_pos()
+			self.app_self.timer.pos["time"] = 5
+
 		self.build()
 	
 	def build(self):
-       		self.app_self.display[self.pos["x"]*self.pos["y"]] = self.skin
+		self.app_self.display[self.pos["x"]-2*self.pos["y"]] = self.skin
 
-class Wall:
-    def __init__(self, app_self):
-        self.app_self = app_self
-        self.app_self.walls.append(self)
-        self.pos = {"x": 2, "y": 3, "width": 2, "height": 2}
-        
-    def loop(self):
-        self.build()
-        
-    def build(self):
-        self.app_self.display[self.pos["x"]*self.pos["y"]] = self.skin
-        
+class Timer:
+	def __init__(self, app_self):
+		self.app_self = app_self
+		self.pos = {"x": 1, "time": 5, "clock": 200}
+		self.skin = 1
+
+	def loop(self):
+		if self.pos["time"] == 0:
+			self.app_self.quit()
+		else:
+			self.build()
+
+			if self.pos["clock"] == 0:
+				self.pos["time"] -= 1
+				self.pos["clock"] = 200
+
+			self.pos["clock"] -= 1
+
+	def build(self):
+		for i in range(self.pos["time"]):
+			self.app_self.display[i] = self.skin
+
+class Coin:
+	def __init__(self, app_self):
+		self.app_self = app_self
+		self.get_pos()
+		self.skin = 5
+
+	def get_pos(self):
+		self.pos = {"x": random.randint(1, 5), "y": random.randint(1, 5)}
+
+	def loop(self):
+		self.build()
+
+	def build(self):
+		self.app_self.display[self.pos["x"]-2*self.pos["y"]] = self.skin
 
 class Main:
 	def __init__(self):
 		self.player = Player(self)
-		self.walls = []
+		self.timer = Timer(self)
+		self.coin = Coin(self)
+
+		self.display = [i-i for i in range(25)]
 		self.run = True
 		
 		self.main()
@@ -44,14 +79,19 @@ class Main:
 	def main(self):
 		while self.run:
 			self.player.loop()
-			
-			for wall in self.walls:
-			    wall.loop()
+			self.timer.loop()
+			self.coin.loop()
 			
 			self.display_update()
 
 	def display_update(self):
-		self.display = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 		display.show(Image(5, 5, bytearray(self.display)))
+		self.display = [i-i for i in range(25)]
 
-main = Main()
+	def quit(self):
+		self.run = False
+		display.scroll(self.player.score)
+		display.clear()
+
+while True:
+	main = Main()
